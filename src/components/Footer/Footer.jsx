7bb3613fe11bc1be367d81cc977f8bb7d7b7fae7
Juan1426 <juancruzzcaro1@gmail.com>
2022-06-22 11:42:34 -0300

    iniciado el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+
+import "./Footer.css"
 
 const Footer = () =>{
 
     return(
         <footer>
-            <h2>footer</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" className="navlink_item">Item1</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/" className="navlink_item">Item1</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/" className="navlink_item">Item1</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </footer>
     )
 }
