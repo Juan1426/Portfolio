4f1607477fa0b1c5abcf157dea9c06f2c2538975
Juan1426 <juancruzzcaro1@gmail.com>
2023-08-29 15:47:49 -0300

    almost fiished html

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export const  Home = () => {
     return(
         <div className="main">
-            <main className="main-container">      
+            <main id="main" className="main_container">      
                 <section className="home-hero">
-                    <div className="title-juan">
+                    <div className="title title-juan">
                         <h2 className="name">Welcome! I'm Juan, Frontend Developer</h2>
                     </div>
                     <div className="description">
-                        <p>My name is Juan, I'm a front-end developer based on Buenos Aires, Argentina.I'm passionate about cutting-edge, beutiful interfaces and intuitively designs.</p>                       
+                        <span>My name is Juan, I'm a front-end developer based on Buenos Aires, Argentina.I'm passionate about cutting-edge, beutiful interfaces and intuitively designs.</span>                       
                     </div>    
                 </section>                 
                 <section className="about">
@@ -26,18 +26,55 @@ export const  Home = () => {
                     </div>                   
                 </section>
                     
-                <section className="proyects">    
-                    <div>
-                    <h2 className="title-projects">Projects</h2>
+                <section id="projects" className="projects">    
+                    <div className="title_projects">
+                        <h2 className="title ">Projects</h2>    
+                        <p>Here you will found some of my personal work.</p>    
                     </div>
-                        
-                    <ul className="proyects-list">
-                        <li><Link to={"https://github.com/Juan1426/Pokedex/tree/master"} className="proyect">Pokedex</Link></li>
-                        
-                        <li><Link to={"https://github.com/Juan1426/DiscoLobo"} className="proyect">Disco Lobo</Link></li>
-                        <li className="proyect">#######</li>
-                    </ul>     
-                </section>           
+                    <div className="projects_container">
+                        <div className="proyect_row">
+                            <div className="proyect_row-img">#img</div>
+                            <Link to={"https://github.com/Juan1426/Pokedex/tree/master"} className="proyect_row-link">Pokedex </Link>
+                            <div className="proyect_row-desc">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat fuga ea obcaecati molestiae aperiam quibusdam atque voluptate optio nulla iste!
+                            </div>
+                        </div>
+                        <div className="proyect_row">
+                            <div className="proyect_row-img">#img</div>
+                            <Link to={"https://github.com/Juan1426/DiscoLobo"} className="proyect_row-link">Disco Lobo</Link>
+                            <div className="proyect_row-desc">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat fuga ea obcaecati molestiae aperiam quibusdam atque voluptate optio nulla iste!
+                            </div>
+                        </div>                
+                    </div>     
+                </section>  
+
+                <section className="contact" id="contact">
+                    <div className="contact_hero">
+
+                    </div>
+                    <div className="contact_form-container">
+                        <form action="" method="post" className="contact_form">
+                            <div className="contact_form-field">
+                                <label className="contact_form-label" htmlFor="">Name</label>
+                                <input required placeholder="Enter Your Name..." type="text" className="contact_form-input" />
+                            </div>
+                            <div className="contact_form-field">
+                                <label htmlFor="" className="contact_form-label">Email</label>
+                                <input required placeholder="Enter Your Email" type="text" className="contact_form-input" />
+                            </div>
+                            <div className="contact_form-field">
+                                <label htmlFor="" className="contact_form-label">Email</label>
+                                <input required placeholder="Enter Your Email" type="text" className="contact_form-input" />
+                            </div>
+                            <div className="contact_form-field">
+                                <label htmlFor="">Messeage</label>
+                                <textarea name="" id="" cols="30" rows="10" className="contact_form-input"></textarea>
+                            </div>
+                           <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </section>
             </main>
         </div>
     )
